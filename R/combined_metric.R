46b1d2d88d6cef26bdf05cee2561b1884d95d0ed
Alia Ajina <aliaajina@gmail.com>
2023-05-11 11:39:08 -0700

    Adding annotation to our combined function

--- R/combined_metric.R
+++ R/combined_metric.R
@@ -1,9 +1,12 @@
-#' @param name description
-#' 
+#' @param m modeled data
+#' @param obs observed data
+#' @param wy water year
+#' @return Combined dataset of the correlation of wet and dry days in the observed and modeled data; wet days were considered a day with greater than 10 inches of precip; dry days were considered a day with less than 0.5 inches of precip
+#' @authors Lauren Harris, Mia Guarnieri, Alia Ajina
 
 combined_metric <- function(m, obs, wy){
   
-  #drought days
+  # drought days
   
   d_m_count <- data.frame(model = m, wy = wy) %>% 
     filter(model < 0.5) %>% 
@@ -21,7 +24,7 @@ combined_metric <- function(m, obs, wy){
   
   d_combined_df <- merge(d_m_count, d_o_count, by = "year") 
   
-  #wet days
+  # wet days
   
   w_m_count <- data.frame(model = m, wy = wy) %>% 
     filter(model > 10) %>% 
@@ -39,7 +42,7 @@ combined_metric <- function(m, obs, wy){
   
   w_combined_df <- merge(w_m_count, w_o_count, by = "year") 
   
-  #correlation
+  # correlation
   
   w_cor <- cor(x = w_combined_df$count_m, y = w_combined_df$count_o)
   
