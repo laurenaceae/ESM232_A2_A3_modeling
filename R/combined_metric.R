265a22e511821455066c17419cb3a104af84a4b4
miag_bio <mia@timg.org>
2023-05-15 14:25:10 -0700

    finishing assignment 5

--- R/combined_metric.R
+++ R/combined_metric.R
@@ -1,54 +1,67 @@
 #' @param m modeled data
 #' @param obs observed data
 #' @param wy water year
-#' @return Combined dataset of the correlation of wet and dry days in the observed and modeled data; wet days were considered a day with greater than 10 inches of precip; dry days were considered a day with less than 0.5 inches of precip
+#' @return Combined dataset of the correlation of wet and dry days in the observed and modeled data; wet days were considered a day with greater than 10 mm of streamflow; dry days were considered a day with less than 0.5 mm of streamflow
 #' @authors Lauren Harris, Mia Guarnieri, Alia Ajina
 
 combined_metric <- function(m, obs, wy){
   
   # drought days
   
+  #modeled count
   d_m_count <- data.frame(model = m, wy = wy) %>% 
     filter(model < 0.5) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(d_m_count) = c("year", "count_m")
   
+  #observed count
   d_o_count <- data.frame(obs = obs, wy = wy) %>% 
     filter(obs < 0.5) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(d_o_count) = c("year", "count_o")
   
+  #combine modeled and observed values into one dataframe
   d_combined_df <- merge(d_m_count, d_o_count, by = "year") 
   
   # wet days
   
+  #modeled count
   w_m_count <- data.frame(model = m, wy = wy) %>% 
     filter(model > 10) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(w_m_count) = c("year", "count_m")
   
+  #observed count
   w_o_count <- data.frame(obs = obs, wy = wy) %>% 
     filter(obs > 10) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(w_o_count) = c("year", "count_o")
   
+  #combine modeled and observed values into one dataframe
   w_combined_df <- merge(w_m_count, w_o_count, by = "year") 
   
-  # correlation
-  
+  # correlation of modeled and observed values for wet and dry days
+
   w_cor <- cor(x = w_combined_df$count_m, y = w_combined_df$count_o)
   
   d_cor <- cor(x = d_combined_df$count_m, y = d_combined_df$count_o)
   
+  #sum of correlation coefficients for wet and dry days
   combined_metric <- w_cor + d_cor
   
-  return(combined_metric)
+  #return the metric, unless NA, then return 0
+  ifelse(is.na(combined_metric), return(0), return(combined_metric))
+  
 }
--- R/drought_days.R
+++ R/drought_days.R
@@ -1,29 +1,34 @@
-#' @param d 
-#' @return Combined data frame of observed and model data with total number of days in each year that had less than 0.5 inches of precipitation
+#' @param d Dataframe of observed and modeled streamflow data (in mm). Column names must be "wy" for water year, "model" for modeled values, and "obs" for observed values.
+#' @return Combined data frame of observed and model data with total number of days in each year that had less than 0.5 mm of streamflow, and the difference between observed and model data.
 #' @authors Lauren Harris, Mia Guarnieri, Alia Ajina
-#' 
 
 drought_days <- function(d){
   
+  #get the count of modeled dry days
   m_count <- d %>% 
-    select(wy, model) %>% 
+    select(wy, model) %>%
     filter(model < 0.5) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(m_count) = c("year", "count_m")
   
+  #get the count of observed dry days
   o_count <- d %>% 
     select(wy, obs) %>% 
     filter(obs < 0.5) %>% 
     group_by(wy) %>% 
     summarise(n())
   
+  #rename columns
   colnames(o_count) = c("year", "count_o")
   
+  #combine count of modeled and observed drought days into one dataframe, and then take the difference in a new column
   combined_df <- merge(m_count, o_count, by = "year") %>% 
     mutate(diff = count_m - count_o)
   
+  #return the new dataframe
   return(combined_df)
   
 }
